e0f6af99c5868d601feabf0ff0b7e0b8f0f39460
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-12-31 02:56:09 +0700

    feat: create prisma service

--- services/src/app.module.ts
+++ services/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { PrismaService } from './clients/database/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'testing')
+          .default('development'),
+        DATABASE_URL: Joi.string().required(),
+        PORT: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- services/src/main.ts
+++ services/src/main.ts
@@ -1,8 +1,32 @@
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { PrismaService } from './clients/database/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configSvc = app.get(ConfigService);
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: '*',
+    allowedHeaders: ['Authorization'],
+    credentials: true,
+  });
+  app.enableVersioning({ type: VersioningType.URI });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  const prismaSvc = app.get(PrismaService);
+  await prismaSvc.enableShutdownHooks(app);
+
+  await app.listen(configSvc.get<string>('PORT'));
+  Logger.log(
+    'Server listening at port ' + configSvc.get<string>('PORT'),
+    'App',
+  );
 }
 bootstrap();
